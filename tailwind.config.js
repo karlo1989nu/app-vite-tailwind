306a6e6ce87848370476bd28bce7b38922546d48
karlo1989nu <karlo.navarro@tajamar3695.com>
2025-04-09 18:25:46 +0200

    final, se agrega readmi

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "/src/assets/styles/index.css", // Asegúrate de incluir todos los archivos relevantes
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
