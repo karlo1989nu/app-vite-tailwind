3140d6b89f3fe03b7d7f6169aa909589b532ff16
karlo1989nu <karlo.navarro@tajamar3695.com>
2025-04-09 18:47:37 +0200

    actualizaciones

--- src/components/common/cont-grid.jsx
+++ src/components/common/cont-grid.jsx
@@ -7,8 +7,6 @@ const GridContainer = () => {
       <div className="bg-green-500 text-white p-4 rounded-lg">Elemento 2</div>
       <div className="bg-red-500 text-white p-4 rounded-lg">Elemento 3</div>
       <div className="bg-yellow-500 text-white p-4 rounded-lg">Elemento 4</div>
-      <div className="bg-purple-500 text-white p-4 rounded-lg">Elemento 5</div>
-      <div className="bg-pink-500 text-white p-4 rounded-lg">Elemento 6</div>
     </div>
   );
 };
